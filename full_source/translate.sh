c83d71d8b8da878e93853f9c7707471a888cd6da
ijauregiCMCRC <ijauregi@cmcrc.com>
2019-10-29 12:44:08 +1100

    fixing train_validate

--- full_source/translate.sh
+++ full_source/translate.sh
@@ -1,4 +1,23 @@
-python translate.py -model zh-en_models/BASELINE_IWSLT2015/seed_1/EPOCH_acc_42.39_ppl_24.27_e14.pt -src ../test_out/TED_zh-en/IWSLT15.TED.tst.tc.zh -doc ../doc.txt -output zh-en_models/BASELINE_IWSLT2015/seed_1/test_all.txt -translate_part all -batch_size 1000 -gpu 1
+#python translate.py -model zh-en_models/HAN_join_newCode_plus_RL_0.33/seed_1/EPOCH_acc_43.38_ppl_22.65_e1_num2.pt -src ../zh-en/text_2011_2013.zh -doc ../zh-en/doc_2011_2013.txt -output zh-en_models/HAN_join_newCode_plus_RL_0.33/seed_1/test_2011_2013_ppl.txt -translate_part all -batch_size 1000 -gpu 1
+
+#python translate.py -model zh-en_models/BASELINE_IWSLT2015_newCode/seed_1/EPOCH_acc_42.66_ppl_23.58_e16.pt -src ../zh-en/text_2011_2013.zh -doc ../zh-en/doc_2011_2013.txt -output zh-en_models/BASELINE_IWSLT2015_newCode/seed_1/test_2011_2013_acc.txt -translate_part all -batch_size 1000 -gpu 1
+
+#python translate.py -model zh-en_models/HAN_join_newCode/acc/seed_1/EPOCH_acc_43.37_ppl_22.51_e1.pt -src ../zh-en/text_2011_2013.zh -doc ../zh-en/doc_2011_2013.txt -output zh-en_models/HAN_join_newCode/acc/seed_1/test_2011_2013.txt -translate_part all -batch_size 1000 -gpu 1
+
+#python translate.py -model zh-en_models/HAN_join_newCode/ppl/seed_1/EPOCH_acc_43.17_ppl_22.94_e1.pt -src ../zh-en/text_2011_2013.zh -doc ../zh-en/doc_2011_2013.txt -output zh-en_models/HAN_join_newCode/ppl/seed_1/test_2011_2013.txt -translate_part all -batch_size 1000 -gpu 1
+
+#LC
+#python ../scripts/LC_RC.py 1 zh-en_models/HAN_join_newCode_plus_RL_0.33/seed_1/test_2011_2013_ppl.txt ../zh-en/doc_2011_2013.txt > zh-en_models/HAN_join_newCode_plus_RL_0.33/seed_1/LC_2011_2013_ppl.txt
+
+#python ../scripts/LC_RC.py 1 zh-en_models/BASELINE_IWSLT2015_newCode/seed_1/test_2011_2013_ppl.txt ../zh-en/doc_2011_2013.txt > zh-en_models/BASELINE_IWSLT2015_newCode/seed_1/LC_2011_2013_ppl.txt
+
+#python ../scripts/LC_RC.py 1 zh-en_models/HAN_join_newCode/ppl/seed_1/test_2011_2013.txt ../zh-en/doc_2011_2013.txt > zh-en_models/HAN_join_newCode/ppl/seed_1/LC_2011_2013.txt
+
+#python ../scripts/LC_RC.py 1 zh-en_models/HAN_join_newCode/acc/seed_1/test_2011_2013.txt ../zh-en/doc_2011_2013.txt > zh-en_models/HAN_join_newCode/acc/seed_1/LC_2011_2013.txt
+
+#Coherence
+python ../scripts/compute_coherence.py 1 zh-en_models/HAN_join_newCode_plus_RL_0.33/seed_1/test_2011_2013_ppl.txt ../zh-en/doc_2011_2013.txt > zh-en_models/HAN_join_newCode_plus_RL_0.33/seed_1/coherence_2011_2013_ppl.txt
+
 
 #BASELINE
 
--- scripts/merge_test_docs.py
+++ scripts/merge_test_docs.py
@@ -1,46 +1,56 @@
 
 #Open text and doc
-test_2010 = open('zh-en/IWSLT15.TED.tst2010.tc.en')
-doc_2010 = open('zh-en/IWSLT15.TED.tst2010.zh-en.doc')
+#test_2010 = open('zh-en/IWSLT15.TED.tst2010.tc.en')
+#doc_2010 = open('zh-en/IWSLT15.TED.tst2010.zh-en.doc')
 
-test_2011 = open('zh-en/IWSLT15.TED.tst2011.tc.en')
+test_2011_zh = open('zh-en/IWSLT15.TED.tst2011.tc.zh')
+test_2011_en = open('zh-en/IWSLT15.TED.tst2011.tc.en')
 doc_2011 = open('zh-en/IWSLT15.TED.tst2011.zh-en.doc')
 
-test_2012 = open('zh-en/IWSLT15.TED.tst2012.tc.en')
+test_2012_zh = open('zh-en/IWSLT15.TED.tst2012.tc.zh')
+test_2012_en = open('zh-en/IWSLT15.TED.tst2012.tc.en')
 doc_2012 = open('zh-en/IWSLT15.TED.tst2012.zh-en.doc')
 
-test_2013 = open('zh-en/IWSLT15.TED.tst2013.tc.en')
+test_2013_zh = open('zh-en/IWSLT15.TED.tst2013.tc.zh')
+test_2013_en = open('zh-en/IWSLT15.TED.tst2013.tc.en')
 doc_2013 = open('zh-en/IWSLT15.TED.tst2013.zh-en.doc')
 
-counter_2010=0
-list_test_2010 = []
-for line in test_2010:
-    list_test_2010.append(line)
-    counter_2010+=1
-list_doc_2010 = []
-for line in doc_2010:
-    num = int(line.strip())
-    list_doc_2010.append(num)
+#counter_2010=0
+#list_test_2010 = []
+#for line in test_2010:
+#    list_test_2010.append(line)
+#    counter_2010+=1
+#list_doc_2010 = []
+#for line in doc_2010:
+#    num = int(line.strip())
+#    list_doc_2010.append(num)
 
 
 counter_2011=0
-list_test_2011 = []
-for line in test_2011:
-    list_test_2011.append(line)
+list_test_2011_en = []
+for line in test_2011_en:
+    list_test_2011_en.append(line)
     counter_2011+=1
+list_test_2011_zh = []
+for line in test_2011_zh:
+    list_test_2011_zh.append(line)
 list_doc_2011 = []
 for line in doc_2011:
     num = int(line.strip())
-    num += counter_2010
+    #num += counter_2010
     list_doc_2011.append(num)
 
-counter_2010_2011 = counter_2010 + counter_2011
+#counter_2010_2011 = counter_2010 + counter_2011
+counter_2010_2011 = counter_2011
 
 counter_2012 = 0
-list_test_2012 = []
-for line in test_2012:
-    list_test_2012.append(line)
+list_test_2012_en = []
+for line in test_2012_en:
+    list_test_2012_en.append(line)
     counter_2012 += 1
+list_test_2012_zh = []
+for line in test_2012_zh:
+    list_test_2012_zh.append(line)
 list_doc_2012 = []
 for line in doc_2012:
     num = int(line.strip())
@@ -51,10 +61,13 @@ counter_2010_2011_2012 = counter_2010_2011 +counter_2012
 
 
 counter_2013 = 0
-list_test_2013 = []
-for line in test_2013:
-    list_test_2013.append(line)
+list_test_2013_en = []
+for line in test_2013_en:
+    list_test_2013_en.append(line)
     counter_2013 += 1
+list_test_2013_zh = []
+for line in test_2013_zh:
+    list_test_2013_zh.append(line)
 list_doc_2013 = []
 for line in doc_2013:
     num = int(line.strip())
@@ -63,15 +76,20 @@ for line in doc_2013:
 
 
 #Total
-total_sents = list_test_2010 + list_test_2011 + list_test_2012 + list_test_2013
-total_docs = list_doc_2010 + list_doc_2011 + list_doc_2012 + list_doc_2013
+total_sents_zh =list_test_2011_zh + list_test_2012_zh + list_test_2013_zh
+total_sents_en =list_test_2011_en + list_test_2012_en + list_test_2013_en
+total_docs = list_doc_2011 + list_doc_2012 + list_doc_2013
 
 
-file_text = open('text.txt','w')
-for elem in total_sents:
+file_text = open('zh-en/text_2011_2013.zh','w')
+for elem in total_sents_zh:
     file_text.write(elem)
 
-file_doc = open('doc.txt', 'w')
+file_text = open('zh-en/text_2011_2013.en','w')
+for elem in total_sents_en:
+    file_text.write(elem)
+
+file_doc = open('zh-en/doc_2011_2013.txt', 'w')
 for elem in total_docs:
     file_doc.write(str(elem)+'\n')
 
